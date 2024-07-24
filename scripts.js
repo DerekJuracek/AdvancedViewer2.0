a5fa7ea543673d0bc9517c998079362fef1da61d
dajuracek@gmail.com <dajuracek@gmail.com>
2024-07-24 11:21:29 -0400

    print height

--- scripts.js
+++ scripts.js
@@ -1338,7 +1338,7 @@ require([
         const pageWidthInInches = 8.5; // Width of the paper in inches
         const pageHeightInInches = 11; // Height of the paper in inches
         const mapWidthInInches = 8; // Slightly reduced width of the map on paper in inches
-        const mapHeightInInches = 7; // Slightly reduced height of the map on paper in inches
+        const mapHeightInInches = 6.5; // Slightly reduced height of the map on paper in inches
         const mapWidthInPixels = mapWidthInInches * printDPI;
         const mapHeightInPixels = mapHeightInInches * printDPI;
 
