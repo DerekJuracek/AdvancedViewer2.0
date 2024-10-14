3b5f34f617a0962ec1e0fab9de4e95afcc7814e8
dajuracek@gmail.com <dajuracek@gmail.com>
2024-10-14 07:09:23 -0400

    print map only test

--- scripts.js
+++ scripts.js
@@ -1596,6 +1596,52 @@ require([
         "Select a Neighborhood"
       );
 
+      //     document
+      //       .getElementById("Print-selector")
+      //       .addEventListener("click", function () {
+      //         captureViewDiv();
+      //       });
+
+      //     function captureViewDiv() {
+      //       const viewDiv = document.getElementById("viewDiv"); // Assuming 'view' is your map container div
+      //       const printWindow = window.open("", "_blank");
+
+      //       // Write the contents of the 'view' div into the new window
+      //       printWindow.document.write(`
+      //   <!DOCTYPE html>
+      //   <html lang="en">
+      //   <head>
+      //     <meta charset="UTF-8">
+      //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      //     <title>Print Map</title>
+      //     <style>
+      //       body {
+      //         margin: 0;
+      //         padding: 0;
+      //         display: flex;
+      //         justify-content: center;
+      //         align-items: center;
+      //       }
+      //       #viewDiv {
+      //         width: 100vw;  /* Set the width of the div to fill the page */
+      //         height: 100vh; /* Set the height of the div to fill the page */
+      //       }
+      //     </style>
+      //   </head>
+      //   <body>
+      //     <div id="viewDiv">${viewDiv.innerHTML}</div> <!-- Copy the contents of the 'view' div -->
+      //     <script>
+      //       window.onload = function() {
+      //         window.print();
+      //       };
+      //     </script>
+      //   </body>
+      //   </html>
+      // `);
+
+      //       printWindow.document.close();
+      //     }
+
       document
         .getElementById("Print-selector")
         .addEventListener("click", function () {
@@ -1619,9 +1665,9 @@ require([
           .then(function (screenshot) {
             const title = "Map Title"; // Set your dynamic title here
             const printWindow = window.open("", "_blank");
-            const scaleBar1 = document.getElementById("scale-value");
-            const scaleBarHTML = scaleBar1.innerHTML;
-            const currentDate = new Date().toLocaleString();
+            // const scaleBar1 = document.getElementById("scale-value");
+            // const scaleBarHTML = scaleBar1.innerHTML;
+            // const currentDate = new Date().toLocaleString();
 
             printWindow.document.write(`
           <!DOCTYPE html>
@@ -1640,56 +1686,56 @@ require([
                       margin: 0;
                       padding: 0;
                   }
-                  .print-title {
-                      display: flex;
-                      align-items: center;
-                      justify-content: center;
-                      text-align: center;
-                      font-size: 24px;
-                      margin: 20px 0;
-                  }
-                  .print-scale {
-                      display: flex;
-                      align-items: center;
-                      justify-content: space-around;
-                      text-align: center;
-                      font-size: 14px;
-                      width: 100%;
-                      margin-left: 20px;
-                      margin-right: 20px;
-                  }
-                  .print-title img {
-                      margin-right: 20px;
-                  }
-                  .scale-bar-container {
-                      transform: scale(1.5);
-                      margin-right: 50px;
-                  }
-                  .print-scale-bar {
-                      width: 300px;
-                      height: 30px;
-                  }
-                  .info-writing-container {
-                      display: flex;
-                      justify-content: center;
-                      align-items: flex-start;
-                      width: 80%;
-                      margin: 20px auto;
-                  }
-                  .info-text {
-                      width: 20%;
-                      text-align: left;
-                      margin-right: 20px;
-                  }
-                  .writing-lines {
-                      width: 80%;
-                      text-align: center;
-                  }
-                  .writing-lines div {
-                      border-bottom: 1px solid black;
-                      margin: 10px 0;
-                      height: 20px;
-                  }
+                  // .print-title {
+                  //     display: flex;
+                  //     align-items: center;
+                  //     justify-content: center;
+                  //     text-align: center;
+                  //     font-size: 24px;
+                  //     margin: 20px 0;
+                  // }
+                  // .print-scale {
+                  //     display: flex;
+                  //     align-items: center;
+                  //     justify-content: space-around;
+                  //     text-align: center;
+                  //     font-size: 14px;
+                  //     width: 100%;
+                  //     margin-left: 20px;
+                  //     margin-right: 20px;
+                  // }
+                  // .print-title img {
+                  //     margin-right: 20px;
+                  // }
+                  // .scale-bar-container {
+                  //     transform: scale(1.5);
+                  //     margin-right: 50px;
+                  // }
+                  // .print-scale-bar {
+                  //     width: 300px;
+                  //     height: 30px;
+                  // }
+                  // .info-writing-container {
+                  //     display: flex;
+                  //     justify-content: center;
+                  //     align-items: flex-start;
+                  //     width: 80%;
+                  //     margin: 20px auto;
+                  // }
+                  // .info-text {
+                  //     width: 20%;
+                  //     text-align: left;
+                  //     margin-right: 20px;
+                  // }
+                  // .writing-lines {
+                  //     width: 80%;
+                  //     text-align: center;
+                  // }
+                  // .writing-lines div {
+                  //     border-bottom: 1px solid black;
+                  //     margin: 10px 0;
+                  //     height: 20px;
+                  // }
                   @media print {
                       body * {
                           visibility: visible;
@@ -1698,29 +1744,11 @@ require([
               </style>
           </head>
           <body>
-              <div class="print-title" id="print-title">
-                  <img id="town-logo" src="${configVars.welcomeImage}" alt="Town Logo">
-                  <h1 id="title-text">${configVars.title}</h1>
-              </div>
+             
               <div class="print-map">
                   <img id="print-map-image" src="${screenshot.dataUrl}" alt="Map Image" style="width: ${mapWidthInInches}in; height: auto; border: 3px solid #A9A9A9; margin: 0 0.75in;">
               </div>
-              <div class="print-scale">
-                  <div class="print-date" style="font-size: 12px;">Date Printed: ${currentDate}</div>
-                  <div id="to-scale" class="scale-bar-container"></div>
-                  <div id="print-scale-bar" class="scale-bar-container">${scaleBarHTML}</div>
-              </div>
-              <div style="text-align: center; font-size: 12px; padding-left: 30px; padding-right: 30px;">
-                  <p>Disclaimer: This map is intended for reference and general informational purposes
-                  only and is not a legally recorded map or survey. While reasonable effort has been
-                  made to ensure the accuracy, correctness, and timeliness of materials presented,
-                  the map vendor and the municipality disclaim any and all liability and responsibility for
-                  any errors, omissions, or inaccuracies in the data provided, including without limitation
-                  any liability for direct, indirect, incidental, consequential, special, exemplary,
-                  punitive, or any other type of damages. Users are hereby notified that the primary
-                  information source should be consulted for verification of the data contained herein.
-                  Continued use of this map acknowledges acceptance of these terms.</p>
-              </div>
+            
               <script>
                   window.onload = function() {
                       window.print();
