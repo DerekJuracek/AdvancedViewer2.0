a4ac91549e38aaf61cea425f9ff887302362c4b3
Derek Juracek <dajuracek@gmail.com>
2025-03-11 14:25:16 -0400

    6.5 & 6.5

--- scripts.js
+++ scripts.js
@@ -1635,8 +1635,8 @@ require([
         const printDPI = 300; // Standard print DPI
         const pageWidthInInches = 8.5; 
         const pageHeightInInches = 11; 
-        const mapWidthInInches = 8; 
-        const mapHeightInInches = 8; 
+        const mapWidthInInches = 6.5; 
+        const mapHeightInInches = 6.5; 
         const mapWidthInPixels = mapWidthInInches * printDPI;
         const mapHeightInPixels = mapHeightInInches * printDPI;
 
@@ -1732,7 +1732,7 @@ require([
                     <h1 id="title-text">${configVars.title}</h1>
                 </div>
                 <div class="print-map">
-                    <img id="print-map-image" src="${screenshot.dataUrl}" alt="Map Image" style="width: ${mapWidthInInches}in; height: auto; border: 3px solid #A9A9A9; margin: 0 0.75in;">
+                    <img id="print-map-image" src="${screenshot.dataUrl}" alt="Map Image" style="width: ${mapWidthInInches}in; height: ${mapHeightInInches}; border: 3px solid #A9A9A9; margin: 0 0.75in;">
                 </div>
                 <div class="print-scale">
                     <div class="print-date" style="font-size: 12px;">Date Printed: ${currentDate}</div>
