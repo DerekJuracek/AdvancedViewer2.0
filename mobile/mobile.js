9bd8ceeffc7c9dd760891c8308ab9bd8fc4edef5
Derek Juracek <dajuracek@gmail.com>
2025-07-16 11:06:30 -0400

    hard code webmap id mobile, mobile layout

---
+++ mobile/mobile.js
@@ -0,0 +1,132 @@
+require([
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+  "esri/core/reactiveUtils",
+  "esri/Graphic",
+  "esri/layers/GraphicsLayer",
+  "esri/widgets/BasemapLayerList",
+  "esri/widgets/Legend",
+
+], function (
+  WebMap,
+  MapView,
+  FeatureLayer,
+  reactiveUtils,
+  Graphic,
+  GraphicsLayer,
+  BasemapLayerList,
+  Legend,
+) {
+  const urlParams = new URLSearchParams(window.location.search);
+  let currentURL = window.location.href;
+  let configUrl = urlParams.get("viewer");
+
+  const configFiles = [
+    "colebrookct",
+    "columbiact",
+    "durhamct",
+    "franklinct",
+    "haddamct",
+    "northcanaanct",
+    "roxburyct",
+    "roxburyctassessor",
+    "scotlandct",
+    "warrenct",
+    "warrenctassessor",
+    "washingtonct",
+    "wiltonct",
+    "wolcottct",
+  ];
+
+  // Create a regex pattern to match allowed config file names
+  let allowedNamesPattern = configFiles.join("|");
+  let urlPattern = new RegExp(`\\?viewer=cama\\/(${allowedNamesPattern})(\\&\\w+=[\\w-]+)*$`);
+  
+
+  if (configUrl != null && urlPattern.test(currentURL)) {
+    configUrl = configUrl + ".json";
+    //$("#whole-app").show();
+  } else if (
+    window.location.href === "https://terrenogis.com" ||
+    window.location.href === "https://terrenogis.com/"
+  ) {
+    // Navigate to the specified URL in the current tab
+    window.location.href = "https://www.qds.biz/gis-service";
+  } else {
+    window.location.href = "./onload.html";
+  }
+
+  const configVars = {
+    mapId: "",
+    condoLayer: "",
+    title: "",
+    isCondosLayer: "",
+    noCondoLayer: "",
+  };
+
+  fetch(configUrl)
+    .then((response) => response.json())
+    .then((config) => {
+      const layers = config.layers;
+      configVars.mapId = config.webmapId || "6448b08504de4244973a28305b18271f";
+      configVars.condoLayer = config.condoLayer;
+      configVars.noCondoLayer = config.noCondoLayer;
+      configVars.title = config.title;
+      configVars.isCondosLayer = config.condos;
+      configVars.masterTable = config.masterTable;
+      configVars.zoom = config.zoom;
+      configVars.parcelZoom = config.parcelZoom;
+      configVars.imageUrl = config.imageUrl;
+      configVars.welcomeImage = config.welcomeImage;
+      configVars.pdf_Image = config.pdf_Image;
+      configVars.pdf_demo = config.pdf_demographics;
+      configVars.taxMap_Url = config.taxMapUrl;
+      configVars.parcelMapUrl = config.parcelMapUrl;
+      configVars.housingUrl = config.housingUrl;
+      configVars.propertyCardPdf = config.propertyCardPdf;
+      configVars.propertyCard = config.propertyCard;
+      configVars.tax_bill = config.tax_bill;
+      configVars.useVisionForTaxBillUrl = config.useVisionForTaxBillUrl;
+      configVars.accessorName = config.accessorName;
+      configVars.parcelTitle = config.parcelServiceTitle;
+      configVars.tabTitle = config.tabTitle;
+      configVars.basemapTitle = config.basemapTitle;
+      configVars.parcelRenderer = config.parcelRenderer;
+      configVars.useUniqueIdforParcelMap = config.useUniqueIdforParcelMap;
+      configVars.helpUrl = config.helpUrl;
+      configVars.includeFilter = config.includeFilter;
+      configVars.customWelcomePage = config.customWelcomePage;
+      configVars.customWelcomeMessage = config.customWelcomeMessage;
+      configVars.showDisclaimer = config.showDisclaimer;
+      configVars.customDisclaimerPage = config.customDisclaimerPage;
+      configVars.customDisclaimerMessage = config.customDisclaimerMessage;
+      configVars.DetailLinks = config.DetailLinks;
+      configVars.DetailLinksToInclude = config.DetailLinksToInclude;
+      configVars.includePermitLink = config.includePermitLink;
+      configVars.scale = config.scale;
+
+    });
+
+    const searchGraphicsLayers = new GraphicsLayer();
+
+      const webmap = new WebMap({
+        portalItem: {
+          id: 'a9b7b11618104b5284db5ef5d7369d95',
+        },
+        layers: [searchGraphicsLayers],
+      });
+
+      var view = new MapView({
+        container: "viewDiv",
+        map: webmap,
+        units: "imperial",
+        scale: configVars.scale,
+        //zoom: `${configVars.zoom}`,
+        popupEnabled: false,
+        ui: {
+          components: ["attribution"],
+        },
+      });
+
+})
